0add12ff4227a527e25f65c7a1b3310bc2c85243
Jihye <parkjh6567@naver.com>
2026-08-04 12:53:23 +0900

    260804_frontend file add and related folder and file add

--- backend/app/schemas/cart_item_schemas.py
+++ backend/app/schemas/cart_item_schemas.py
@@ -3,11 +3,10 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
 
-
 class CartItemsPublic(BaseModel):
     # DB에서 조회한 상품을 API 응답으로 보낼 때 사용하는 형식입니다. 
     id : UUID = Field(default_factory=uuid4)
-    product_id : int
+    product_id : str
     product_name :str
     quantity :  int = Field(ge=1, examples=[1])
     created_at : datetime
@@ -16,7 +15,7 @@ class CartItemsPublic(BaseModel):
 class CartItemsCreate(BaseModel):
     # 장바구니 생성 요청에서 반드시 받아야 하는 값입니다. 
     id : UUID = Field(default_factory=uuid4)
-    product_id : int
+    product_id : str
     product_name :str
     quantity :  int = Field(ge=1, examples=[1])
     created_at : datetime
@@ -24,7 +23,7 @@ class CartItemsCreate(BaseModel):
 class CartItemsUpdate(BaseModel):
     # 장바구니 수정 요청에서 받을 값입니다. 
     id : UUID = Field(default_factory=uuid4)
-    product_id : int
+    product_id : str
     product_name :str
     quantity :  int = Field(ge=1, examples=[1])
     updated_at : datetime
